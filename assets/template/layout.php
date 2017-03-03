fb090ba99c16691bbef71feb50abfbfdb5eeb99b
Aleksandr Novikov <pwsdotru@gmail.com>
2017-03-03 21:51:31 +0300

    Add support for twitter bootstrap

--- assets/template/layout.php
+++ assets/template/layout.php
@@ -1,10 +1,21 @@
 <!DOCTYPE html>
-<html>
+<html lang="ru">
 <head>
-	<title>PHPixie</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Fetcher</title>
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+	<link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+	<link href="/assets/css/main.css" rel="stylesheet">
 </head>
-<body>
-<h1>PHPixie</h1>
-<?php $this->childContent();?>
+<body id="top">
+	<h1>PHPixie</h1>
+	<?php $this->childContent();?>
 </body>
 </html>
